ddf58d4f644736bf76fda33f0b8740f3028eade6
PedroNavarrete200 <80926009+PedroNavarrete200@users.noreply.github.com>
2021-03-24 17:39:57 -0600

    Add files via upload

---
+++ Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+var v1 = "Esto es Swift"
+let c1 = 4
+var cadena:String = String()
+cadena = "hola mundo" + v1
+print("el contenido es: \(cadena)")
+print("el valor de constante es: \(c1)")
